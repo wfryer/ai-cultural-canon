be8d729b011e2da334d606c711d7c798f4fbb994
wfryer <wesfryer@gmail.com>
2025-11-17 22:09:31 -0500

    Create firebase-config.sample.js

---
+++ firebase-config.sample.js
@@ -0,0 +1,11 @@
+// firebase-config.sample.js
+// Copy this file to firebase-config.js and paste in YOUR Firebase project settings.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
